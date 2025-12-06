76e5f62506304e937873e5b0998bb96ec0d465be
MajoOrtiz <majortiz25@gmail.com>
2025-12-06 17:11:56 -0600

    modificar app commit

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def root():
+    return jsonify({'status': 'ok', 'message': 'API running'}), 200
+
 @app.route('/saludo', methods=['GET'])
 def saludo():
     return jsonify({'mensaje': 'Hola Mundo, como estas, sera, quien sabe pero parece que si'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
