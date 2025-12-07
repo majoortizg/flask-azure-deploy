f77f80037fdf76732502562c3f33098614e0f9db
MajoOrtiz <majortiz25@gmail.com>
2025-12-06 18:52:18 -0600

    cambio en app

--- app.py
+++ app.py
@@ -6,13 +6,9 @@ app = Flask(__name__)
 def root():
     return jsonify({'status': 'ok', 'routes': ['/saludo','/status']}), 200
 
-@app.route('/status', methods=['GET'])
-def status():
-    return jsonify({'status': 'running', 'version': '1.0', 'author': 'Majo Ortiz'}), 200
-
 @app.route('/saludo', methods=['GET'])
 def saludo():
-    return jsonify({'mensaje': 'Mundo, como estas, sera, quien sabe pero parece que si'}), 200
+    return jsonify({'mensaje': 'Hola Profeee :))))), Fabricio el profe mas cool B) '}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
