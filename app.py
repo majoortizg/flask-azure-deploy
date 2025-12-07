da718593c99a24c1dda02df8526ea886e7154ccb
MajoOrtiz <majortiz25@gmail.com>
2025-12-06 21:53:52 -0600

    cambio en app + nuevo endpoint

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def root():
     return jsonify({'status': 'ok', 'routes': ['/saludo','/status']}), 200
 
+@app.route('/status', methods=['GET'])
+def status():
+    return jsonify({'status': 'running', 'version': '1.0', 'author': 'Majo Ortiz'}), 200	
+
 @app.route('/saludo', methods=['GET'])
 def saludo():
-    return jsonify({'mensaje': 'Hola Profeee :)))), Fabricio el profe mas cool B) '}), 200
+    return jsonify({'mensaje': 'Hola Profeee :)))), Fabricio el profe mas cool B) Cambio: Agrego nuevo endpoint'}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
